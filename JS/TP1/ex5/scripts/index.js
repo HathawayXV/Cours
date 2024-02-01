5238a2ac40c89f5802717c80993653c934c87b14
Yannick HAN <134492304+Minyth@users.noreply.github.com>
2024-02-01 21:43:32 +0100

    Add files via upload

--- JS/TP1/ex5/scripts/index.js
+++ JS/TP1/ex5/scripts/index.js
@@ -5,20 +5,16 @@ var abscisse = 0;
 var ordonnee = 0;
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    let tableau = document.querySelector('.ex5');
-    tableau.addEventListener("mousemove", changer_position);
+    let tableau = document.querySelector('body');
+    tableau.addEventListener("mouseup", changer_position);
 });
 
-function changer_position(event) {
-    if(event.buttons){
-        console.log("in");
-        abscisse = event.movementX;
-        ordonnee = event.movementY;
-        event.target.style.left = abscisse + "px";
-        event.target.style.top = ordonnee + "px";
-    }
-    console.log("fin");
-}
 
+function changer_position(event) {
+    abscisse = event.mouvementX;
+    ordonnee = event.mouvementY;
+    console.log("x: " + abscisse + " y: " + ordonnee);
+    
+}
 /*Mémorisé le début, puis détécter la fin puis calculer la distance*/
 /* ------------------------------------------------------------ */
